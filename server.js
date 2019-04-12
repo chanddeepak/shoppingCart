a1c173b1f1e266241713f581515a7097d36324f0
Deepak Chand <deepak.chand@nagarro.com>
2019-04-12 13:11:28 +0530

    commit
    Signed-off-by: Deepak Chand <deepak.chand@nagarro.com>

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/products', products);
 app.use('/users', users);
 app.use('/cart', cart);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 db.sync()
     .then(() => {
